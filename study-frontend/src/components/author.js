f0c5d732cbbae718a636429a7d02b99568138b1e
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-01-09 21:08:57 -0700

    author class cerated with constractor

---
+++ study-frontend/src/components/author.js
@@ -0,0 +1,13 @@
+class Author {
+    constructor(data) {
+        this.id = data.id
+        this.name = data.name
+        this.proverbs = data.proverbs
+    }
+
+
+}
+
+
+//<button id="delete-author" data-id="${this.id}"> Delete </button>
+//afteer return the proverbs
