267823edf29b733a36806dbb609a35b68850a7d2
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-01-09 21:28:46 -0700

    create fetchauthors to bring data from api

--- study-frontend/src/services/ApiService.js
+++ study-frontend/src/services/ApiService.js
@@ -3,5 +3,16 @@ class ApiService {
         this.BASE_URL = 'http://localhost:3000'
     }
 
+    async fetchAuthors() {
+        let res = await fetch(this.BASE_URL + '/authors')
+        let data = await res.json()
+        return data
+    }
+
+    async fetchAuthor(id) {
+        let res = await fetch(this.BASE_URL + `/authors/${id}`)
+        let data = await res.json()
+        return data
+    }
 
 }
